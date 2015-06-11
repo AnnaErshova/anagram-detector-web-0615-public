af0c4133ade3e81fec041284397d835b31e9d191
AnnaErshova <anna.ershova@gmail.com>
2015-06-11 13:06:53 -0400

    all done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,50 +1,48 @@
 # Anagram Detector
 
 ## Instructions
-
 # Write a program that, given a word and a list of possible anagrams,
 # selects the correct one(s).
-
 #Your class, `Anagram` should take a word on initialization, and instances should
 #respond to a `match` method that takes a list of possible anagrams. It should return
 #all matches in an array. If no matches exist, it should return an empty array.
-
 #In other words, given: `"listen"` and `%w(enlists google inlets banana)`
 #the program should return `["inlets"]`.
-
 #Write your solution in anagram.rb. Run the tests with `rspec`.
-
 #PS. [What is %w?](http://stackoverflow.com/questions/1274675/ruby-what-does-warray-mean)
 #PPS. For a hint, `git checkout hints` and read `HINTS.md`.
-## Resources
 #* [StackOverflow](http://stackoverflow.com/) - [Ruby: what does %w(array) mean?](http://stackoverflow.com/questions/1274675/ruby-what-does-warray-mean)
-# Your code goes here!
+
+# take string, sort letters alphabetically; take array of strings, sort letters in string alphabetically, compare the two and return matches in their original form
+# can create a helper method that sorts letters in a string alphabetically
 
 class Anagram
+
+  # word_to_match is a string
 
   def initialize(word_to_match)
     @word_to_match = word_to_match
   end
 
-  # takes a list of possible anagrams
   # returns all matches in an array; if no matches, return empty array
+
+  # input_array is an array of strings of possible anagrams
   def match(input_array)
+    sorted_array = [] # creates an empty array to dump sorted strings into
 
-    sorted_array = []
+    input_array.each do |word|
+      if word.split("").sort == @word_to_match.split("").sort
+        sorted_array << word # word is original form
+      end
+    end # ends block
 
-    input_array.each do |string|
-      sorted_array << string.split("").sort # array of choices
-    end
-
-    sorted_word = @word_to_match.split("").sort.join("") # array to match
-
-    if sorted_array.include?(sorted_word)
-      puts sorted_word
-    end
-    [sorted_word]
-  end # ends match
+    sorted_array
+  end # end match method
 
 end # end class
+
+
+
 
 # In other words, given: `"listen"` and `%w(enlists google inlets banana)`
 # the program should return `["inlets"]`.
